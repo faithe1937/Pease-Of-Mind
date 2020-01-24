733ad274a817dae8fd97e969c99a1aab06751cde
Faith <faithe1937@gmail.com>
2020-01-24 12:41:21 -0600

    chart for macros done

--- frontend/src/components/favorite/Chart.js
+++ frontend/src/components/favorite/Chart.js
@@ -2,31 +2,38 @@ import React, { Component } from "react";
 import { Bar } from "react-chartjs-2";
 
 class Chart extends Component {
-  constructor() {
-    super();
-    this.state = {
-      data: {
-        labels: ["Fats", "Carbohydrates", "Proteins"],
-        datasets: [
-          {
-            label: "Daily Value",
-            data: [835785, 297485, 938578],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.5)",
-              "rgba(54, 162, 235, 0.6)",
-              "rgba(255, 206, 86, 0.6)"
-            ]
-          }
-        ]
-      }
-    };
-  }
 
   render() {
+    console.log(this.props.calories)
+   const data = {
+    labels: ["Calories", "Fat (g)", "Carbohydrate (g)", "Protein (g) "],
+    datasets: [
+      {
+        label: "Daily Value",
+        data: [
+          //calories
+          this.props.calories,
+          //fats 
+          this.props.fat, 
+          //carbs
+          this.props.carb, 
+          //protein
+          this.props.protein],
+
+        backgroundColor: [
+          "rgba(153, 102, 255, 0.6)",
+          "rgba(255, 99, 132, 0.6)",
+          "rgba(54, 162, 235, 0.6)",
+          "rgba(255, 206, 86, 0.6)"
+        ]
+      }
+    ]
+  }
+
     return (
       <div className="chart">
         <Bar
-          data={this.state.data}
+          data={data}
           options={{
             title: {
               display: true,
--- frontend/src/components/favorite/FavContainer.js
+++ frontend/src/components/favorite/FavContainer.js
@@ -3,14 +3,23 @@ import UserInfo from "./UserInfo";
 import Chart from "./Chart";
 
 const FavContainer = props => {
-  // console.log(props.user);
+
+let calories = ((props.user.weight * 6.24) + (props.user.height * 12.7) - (props.user.age * 6.755))
+let fat = ((calories/.30) / (9) )
+let carb = ((calories/.40) / (4))
+let protein = ((calories/.30) / (4)) 
+
   return (
     <div>
       <UserInfo user={props.user} />
       <br />
       <br />
       <br />
-      <Chart userData={props.user} />
+      <Chart userData={props.user} 
+      calories={calories} 
+      fat={fat}
+      carb={carb}
+      protein={protein} />
     </div>
   );
 };
